6878ce54db0b2eb08bcb6f6fe4a1b77f17bf1fa3
blyking <46658092+blyking@users.noreply.github.com>
2020-04-24 13:48:43 -0400

    Update ModelBased.py

--- ModelBased.py
+++ ModelBased.py
@@ -13,7 +13,7 @@ from Pitch import *
 from Putt import *
 import numpy as np
 import random
-import pylab as pl
+#import pylab as pl
 
 # dictionareis to hash indexes to states/actions
 states = {}
@@ -175,7 +175,7 @@ M *= -1
 
 # populate M matrix with reward for each state
 for point in edges:
-    print(point)
+    #print(point)
     if point[1] is goal:
         M[point[0].getNumber(), point[1].getNumber()] = 100
     elif (point[0] is fairway or point[0] is ravine) and (
@@ -196,8 +196,8 @@ for point in edges:
 
 # set goal reward
 M[goal.getNumber(), goal.getNumber() - 1] = 100
-print('\n')
-print(M)
+#print('\n')
+#print(M)
 
 Q = np.matrix(np.zeros([MATRIX_SIZE, MATRIX_SIZE]))
 
@@ -302,9 +302,12 @@ update(initial_state, action, gamma)
 scores = []
 epsilon = 500
 
+trial_matrix = np.matrix(np.ones(shape=(epsilon, 2)))
+
 # run epsilon iterations of the golf game
 for i in range(epsilon):
     current_state = np.random.randint(2, int(Q.shape[0]))
+    trial_matrix[i, 0] = current_state
     if (current_state == 6):
         continue
     available_action = available_actions(current_state)
@@ -312,8 +315,397 @@ for i in range(epsilon):
     #     #scores.append(3.5)
     #     continue
     action = sample_next_action(available_action, current_state)
+    trial_matrix[i, 1] = action[0].getNumber()
     score = update(current_state, action, gamma)
     scores.append(score)
+
+#states:
+#fairway = 0
+#ravine = 1
+#leftOfPin = 2
+#closeToPin = 5
+#inHole = 6
+#sameLevel = 4
+#overTheGreen = 3
+
+#actions:
+#atPin = 1
+#pastPin = 2
+#leftPin = 3
+#chip = 4
+#pitch = 5
+#putt = 6
+
+#statecounts
+numfairway = 0
+numravine = 0
+numleftofpin = 0
+numclosetopin = 0
+numinhole = 0
+numsamelevel = 0
+numoverthegreen = 0
+
+count01 = 0 #fairway-at
+count015 = 0 #fairway-at-close
+count014 = 0 #fairway-at-same
+count013 = 0 #fairway-at-over
+count011 = 0 #fairway-at-ravine
+count012 = 0 #fairway-at-left
+
+count02 = 0 #fairway-past
+count025 = 0 #fairway-past-close
+count024 = 0 #fairway-past-same
+count023 = 0 #fairway-past-over
+count021 = 0 #fairway-past-ravine
+count022 = 0 #fairway-past-left
+
+count03 = 0 #fairway-left
+count035 = 0 #fairway-left-close
+count034 = 0 #fairway-left-same
+count033 = 0 #fairway-left-over
+count031 = 0 #fairway-left-ravine
+count032 = 0 #fairway-left-left
+
+count11 = 0 #ravine-at
+count115 = 0 #ravine-at-close
+count111 = 0 #ravine-at-ravine
+count112 = 0 #ravine-at-left
+count114 = 0 #ravine-at-same
+count113 = 0 #ravine-at-over
+
+count12 = 0 #ravine-past
+count125 = 0 #ravine-past-close
+count121 = 0 #ravine-past-ravine
+count122 = 0 #ravine-past-left
+count124 = 0 #ravine-past-same
+count123 = 0 #ravine-past-over
+
+count13 = 0 #ravine-left
+count135 = 0 #ravine-at-close
+count131 = 0 #ravine-at-ravine
+count132 = 0 #ravine-at-left
+count134 = 0 #ravine-at-same
+count133 = 0 #ravine-at-over
+
+count34 = 0 #over-chip
+count345 = 0 #over-chip-close
+count344 = 0 #over-chip-same
+count341 = 0 #over-chip-ravine
+count343 = 0 #over-chip-over
+count342 = 0 #over-chip-left
+count346 = 0 #over-chip-in
+
+count35 = 0 #over-pitch
+count355 = 0 #over-pitch-close
+count354 = 0 #over-pitch-same
+count351 = 0 #over-pitch-ravine
+count353 = 0 #over-pitch-over
+count352 = 0 #over-pitch-left
+count356 = 0 #over-pitch-in
+
+count46 = 0 #same-putt
+count464 = 0 #same-putt-same
+count465 = 0 #same-putt-close
+count462 = 0 #same-putt-left
+count466 = 0 #same-putt-in
+
+count26 = 0 #left-putt
+count264 = 0 #left-putt-same
+count265 = 0 #left-putt-close
+count262 = 0 #left-putt-left
+count266 = 0 #left-putt-in
+
+count56 = 0 #close-putt
+count565 = 0 #close-putt-close
+count566 = 0 #close-putt-in
+
+initial_states_for_table = np.asmatrix([[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[1],[2],[2],[2],[2],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[3],[4],[4],[4],[4],[5],[5]])
+actions_for_table= np.asmatrix([[1],[1],[1],[1],[1],[2],[2],[2],[2],[2],[3],[3],[3],[3],[3],[1],[1],[1],[1],[1],[2],[2],[2],[2],[2],[3],[3],[3],[3],[3],[6],[6],[6],[6],[4],[4],[4],[4],[4],[4],[5],[5],[5],[5],[5],[5],[6],[6],[6],[6],[6],[6]])
+end_states_for_table= np.asmatrix([[1],[2],[3],[4],[5],[1],[2],[3],[4],[5],[1],[2],[3],[4],[5],[1],[2],[3],[4],[5],[1],[2],[3],[4],[5],[1],[2],[3],[4],[5],[2],[4],[5],[6],[1],[2],[3],[4],[5],[6],[1],[2],[3],[4],[5],[6],[2],[4],[5],[6],[5],[6]])
+
+trans_prob_table = np.matrix(np.ones(shape=(len(end_states_for_table[:,0]), 4)))
+
+#count each time state occurs
+
+for i in range(epsilon-1):
+    if trial_matrix[i, 0] == 0: #start state
+        numfairway += 1
+        if trial_matrix[i, 1] == 1: #action
+            count01 += 1
+            if trial_matrix[i+1, 0] == 1: #end state
+                count011 += 1
+            elif trial_matrix[i+1, 0] == 2:
+                count012 += 1
+            elif trial_matrix[i+1, 0] == 3:
+                count013 += 1
+            elif trial_matrix[i+1, 0] == 4:
+                count014 += 1
+            elif trial_matrix[i+1, 0] == 5:
+                count015 += 1
+        elif trial_matrix[i, 1] == 2:
+            count02 += 1
+            if trial_matrix[i + 1, 0] == 1:  #end state
+                count021 += 1
+            elif trial_matrix[i + 1, 0] == 2:
+                count022 += 1
+            elif trial_matrix[i + 1, 0] == 3:
+                count023 += 1
+            elif trial_matrix[i + 1, 0] == 4:
+                count024 += 1
+            elif trial_matrix[i + 1, 0] == 5:
+                count025 += 1
+        if trial_matrix[i, 1] == 3:
+            count03 += 1
+            if trial_matrix[i + 1, 0] == 1:  # end state
+                count031 += 1
+            elif trial_matrix[i + 1, 0] == 2:
+                count032 += 1
+            elif trial_matrix[i + 1, 0] == 3:
+                count033 += 1
+            elif trial_matrix[i + 1, 0] == 4:
+                count034 += 1
+            elif trial_matrix[i + 1, 0] == 5:
+                count035 += 1
+    elif trial_matrix[i, 0] == 1:
+        numravine += 1
+        if trial_matrix[i, 1] == 1:
+            count11 += 1
+            if trial_matrix[i+1, 0] == 1:
+                count111 += 1
+            elif trial_matrix[i+1, 0] == 2:
+                count112 += 1
+            elif trial_matrix[i+1, 0] == 3:
+                count113 += 1
+            elif trial_matrix[i+1, 0] == 4:
+                count114 += 1
+            elif trial_matrix[i+1, 0] == 5:
+                count115 += 1
+        elif trial_matrix[i, 1] == 2:
+            count12 += 1
+            if trial_matrix[i+1, 0] == 1:
+                count121 += 1
+            elif trial_matrix[i+1, 0] == 2:
+                count122 += 1
+            elif trial_matrix[i+1, 0] == 3:
+                count123 += 1
+            elif trial_matrix[i+1, 0] == 4:
+                count124 += 1
+            elif trial_matrix[i+1, 0] == 5:
+                count125 += 1
+        elif trial_matrix[i, 1] == 3:
+            count13 += 1
+            if trial_matrix[i+1, 0] == 1:
+                count131 += 1
+            elif trial_matrix[i+1, 0] == 2:
+                count132 += 1
+            elif trial_matrix[i+1, 0] == 3:
+                count133 += 1
+            elif trial_matrix[i+1, 0] == 4:
+                count134 += 1
+            elif trial_matrix[i+1, 0] == 5:
+                count135 += 1
+    elif trial_matrix[i, 0] == 2:
+        numleftofpin += 1
+        if trial_matrix[i, 1] == 6:
+            count26 += 1
+            if trial_matrix[i+1, 0] == 2:
+                count262 += 1
+            elif trial_matrix[i+1, 0] == 4:
+                count264 += 1
+            elif trial_matrix[i+1, 0] == 5:
+                count265 += 1
+            elif trial_matrix[i+1, 0] == 6:
+                count266 += 1
+    elif trial_matrix[i, 0] == 3:
+        numoverthegreen += 1
+        if trial_matrix[i, 1] == 4:
+            count34 += 1
+            if trial_matrix[i+1, 0] == 1:
+                count341 += 1
+            elif trial_matrix[i+1, 0] == 2:
+                count342 += 1
+            elif trial_matrix[i+1, 0] == 3:
+                count343 += 1
+            elif trial_matrix[i+1, 0] == 4:
+                count344 += 1
+            elif trial_matrix[i+1, 0] == 5:
+                count345 += 1
+            elif trial_matrix[i+1, 0] == 6:
+                count346 += 1
+        elif trial_matrix[i, 1] == 5:
+            count35 += 1
+            if trial_matrix[i+1, 0] == 1:
+                count351 += 1
+            elif trial_matrix[i+1, 0] == 2:
+                count352 += 1
+            elif trial_matrix[i+1, 0] == 3:
+                count353 += 1
+            if trial_matrix[i+1, 0] == 4:
+                count354 += 1
+            elif trial_matrix[i+1, 0] == 5:
+                count355 += 1
+            elif trial_matrix[i+1, 0] == 6:
+                count356 += 1
+    elif trial_matrix[i, 0] == 4:
+        numsamelevel += 1
+        if trial_matrix[i, 1] == 6:
+            count46 += 1
+            if trial_matrix[i+1, 0] == 2:
+                count462 += 1
+            elif trial_matrix[i+1, 0] == 4:
+                count464 += 1
+            elif trial_matrix[i+1, 0] == 5:
+                count465 += 1
+            elif trial_matrix[i+1, 0] == 6:
+                count466 += 1
+    elif trial_matrix[i, 0] == 5:
+        numclosetopin += 1
+        if trial_matrix[i, 1] == 6:
+            count56 += 1
+            if trial_matrix[i+1, 0] == 5:
+                count565 += 1
+            elif trial_matrix[i+1, 0] == 6:
+                count566 += 1
+    elif trial_matrix[i, 0] == 6:
+        numinhole += 1
+
+trans_prob_table[:,0] = initial_states_for_table
+trans_prob_table[:,1] = actions_for_table
+trans_prob_table[:,2] = end_states_for_table
+
+def findProb(B, A):
+    if (A > 0):
+        return B / A
+    else:
+        return A
+
+for i in range(len(trans_prob_table[:,0])):
+    if trans_prob_table[i,0] == 0:
+        if trans_prob_table[i,1] == 1:
+            if trans_prob_table[i,2] == 1:
+                trans_prob_table[i,3] = findProb(count011, count01)
+            elif trans_prob_table[i,2] == 2:
+               trans_prob_table[i,3] = findProb(count012, count01)
+            elif trans_prob_table[i,2] == 3:
+                trans_prob_table[i,3] = findProb(count013, count01)
+            elif trans_prob_table[i,2] == 4:
+                trans_prob_table[i,3] = findProb(count014, count01)
+            elif trans_prob_table[i,2] == 5:
+                trans_prob_table[i,3] = findProb(count015, count01)
+        elif trans_prob_table[i,1] == 2:
+            if trans_prob_table[i, 2] == 1:
+                trans_prob_table[i, 3] = findProb(count021, count02)
+            elif trans_prob_table[i, 2] == 2:
+                trans_prob_table[i, 3] = findProb(count022, count02)
+            elif trans_prob_table[i, 2] == 3:
+                trans_prob_table[i, 3] = findProb(count023, count02)
+            elif trans_prob_table[i, 2] == 4:
+                trans_prob_table[i, 3] = findProb(count024, count02)
+            elif trans_prob_table[i, 2] == 5:
+                trans_prob_table[i, 3] = findProb(count025, count02)
+        elif trans_prob_table[i,1] == 3:
+            if trans_prob_table[i,2] == 1:
+                trans_prob_table[i,3] = findProb(count031, count03)
+            elif trans_prob_table[i,2] == 2:
+                trans_prob_table[i,3] = findProb(count032, count03)
+            elif trans_prob_table[i,2] == 3:
+                trans_prob_table[i,3] = findProb(count033, count03)
+            elif trans_prob_table[i,2] == 4:
+                trans_prob_table[i,3] = findProb(count034, count03)
+            elif trans_prob_table[i,2] == 5:
+                trans_prob_table[i,3] = findProb(count035, count03)
+    elif trans_prob_table[i,0] == 1:
+        if trans_prob_table[i,1] == 1:
+            if trans_prob_table[i,2] == 1:
+                trans_prob_table[i,3] = findProb(count111, count11)
+            elif trans_prob_table[i,2] == 2:
+                trans_prob_table[i,3] = findProb(count112, count11)
+            elif trans_prob_table[i,2] == 3:
+                trans_prob_table[i,3] = findProb(count113, count11)
+            elif trans_prob_table[i,2] == 4:
+                trans_prob_table[i,3] = findProb(count114, count11)
+            elif trans_prob_table[i,2] == 5:
+                trans_prob_table[i,3] = findProb(count115, count11)
+        elif trans_prob_table[i,1] == 2:
+            if trans_prob_table[i, 2] == 1:
+                trans_prob_table[i, 3] = findProb(count121, count12)
+            elif trans_prob_table[i, 2] == 2:
+                trans_prob_table[i, 3] = findProb(count122, count12)
+            elif trans_prob_table[i, 2] == 3:
+                trans_prob_table[i, 3] = findProb(count123, count12)
+            elif trans_prob_table[i, 2] == 4:
+                trans_prob_table[i, 3] = findProb(count124, count12)
+            elif trans_prob_table[i, 2] == 5:
+                trans_prob_table[i, 3] = findProb(count125, count12)
+        elif trans_prob_table[i,1] == 3:
+            if trans_prob_table[i,2] == 1:
+                trans_prob_table[i,3] = findProb(count131, count13)
+            elif trans_prob_table[i,2] == 2:
+                trans_prob_table[i,3] = findProb(count132, count13)
+            elif trans_prob_table[i,2] == 3:
+                trans_prob_table[i,3] = findProb(count133, count13)
+            elif trans_prob_table[i,2] == 4:
+                trans_prob_table[i,3] = findProb(count134, count13)
+            elif trans_prob_table[i,2] == 5:
+                trans_prob_table[i,3] = findProb(count135, count13)
+    elif trans_prob_table[i, 0] == 2:
+        if trans_prob_table[i,1] == 6:
+            if trans_prob_table[i,2] == 2:
+                trans_prob_table[i,3] = findProb(count262, count26)
+            elif trans_prob_table[i,2] == 4:
+                trans_prob_table[i,3] = findProb(count264, count26)
+            elif trans_prob_table[i,2] == 5:
+                trans_prob_table[i,3] = findProb(count265, count26)
+            elif trans_prob_table[i,2] == 6:
+                trans_prob_table[i,3] = findProb(count266, count26)
+    elif trans_prob_table[i, 0] == 3:
+        if trans_prob_table[i, 1] == 4:
+            if trans_prob_table[i,2] == 1:
+                trans_prob_table[i,3] = findProb(count341, count34)
+            elif trans_prob_table[i,2] == 2:
+                trans_prob_table[i,3] = findProb(count342, count34)
+            elif trans_prob_table[i,2] == 3:
+                trans_prob_table[i,3] = findProb(count343, count34)
+            elif trans_prob_table[i,2] == 4:
+                trans_prob_table[i,3] = findProb(count344, count34)
+            elif trans_prob_table[i,2] == 5:
+                trans_prob_table[i,3] = findProb(count345, count34)
+            elif trans_prob_table[i,2] == 6:
+                trans_prob_table[i,3] = findProb(count346, count34)
+        elif trans_prob_table[i, 1] == 5:
+            if trans_prob_table[i,2] == 1:
+                trans_prob_table[i,3] = findProb(count351, count35)
+            if trans_prob_table[i,2] == 2:
+                trans_prob_table[i,3] = findProb(count352, count35)
+            if trans_prob_table[i,2] == 3:
+                trans_prob_table[i,3] = findProb(count353, count35)
+            if trans_prob_table[i,2] == 4:
+                trans_prob_table[i,3] = findProb(count354, count35)
+            if trans_prob_table[i,2] == 5:
+                trans_prob_table[i,3] = findProb(count355, count35)
+            if trans_prob_table[i,2] == 6:
+                trans_prob_table[i,3] = findProb(count356, count35)
+    elif trans_prob_table[i, 0] == 4:
+        if trans_prob_table[i, 1] == 6:
+            if trans_prob_table[i, 2] == 2:
+                trans_prob_table[i, 3] = findProb(count462, count46)
+            elif trans_prob_table[i, 2] == 4:
+                trans_prob_table[i, 3] = findProb(count464, count46)
+            elif trans_prob_table[i, 2] == 5:
+                trans_prob_table[i, 3] = findProb(count465, count46)
+            elif trans_prob_table[i, 2] == 6:
+                trans_prob_table[i, 3] = findProb(count466, count46)
+    elif trans_prob_table[i, 0] == 5:
+        if trans_prob_table[i, 1] == 6:
+            if trans_prob_table[i, 2] == 5:
+                trans_prob_table[i, 3] = findProb(count565, count56)
+            if trans_prob_table[i, 2] == 6:
+                trans_prob_table[i, 3] = findProb(count566, count56)
+
+
+print("Transitional Probability Table")
+print("[State State], [Action], [End State], [Probability Table]")
+print(trans_prob_table)
 
 # show trained matrix. Matrix contains the reqard/utilites for each state/action combination
 print("Trained Q matrix:")
@@ -325,7 +717,7 @@ steps = [current_state]
 
 # generate the best path to take
 while current_state != 5:
-    print(current_state)
+    #print(current_state)
     next_step_index = np.where(Q[current_state,] == np.max(Q[current_state,]))[1]
 
     if next_step_index.shape[0] > 1:
@@ -338,7 +730,7 @@ while current_state != 5:
 print("Most efficient path:")
 print(steps)
 
-# show graph of the score acheived over time
+#show graph of the score acheived over time
 pl.plot(scores)
 pl.xlabel('No of iterations')
 pl.ylabel('Reward gained')
